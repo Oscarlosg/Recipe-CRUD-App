c4fab5eb807c46bbe6b8cf4bed77e46f7c316709
oscarlos Gomez Rosario <oscarlosg14@hotmail.com>
2022-06-26 19:15:20 -0400

    first commit

---
+++ src/RecipeCreate.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+
+function RecipeCreate({ recipes, setRecipes }) {
+  const [NewRecipe, setNewRecipe] = useState({
+    // state to store form data so it can be passed to the master recipes state.
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  });
+
+  function handleChange(event) {
+    // adds inputted data into the corresponding key in the new recipe state
+    const { target } = event;
+    setNewRecipe({ ...NewRecipe, [target.name]: target.value });
+  }
+
+  function handleSubmit(event) {
+    // adds new recipe to the master recipe state and resets state to its original state once the data has been passed to the master recipe state
+    event.preventDefault();
+    const { target } = event;
+    setRecipes([...recipes, NewRecipe]);
+    setNewRecipe({
+      name: "",
+      cuisine: "",
+      photo: "",
+      ingredients: "",
+      preparation: "",
+    });
+  }
+
+  // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
+  // TODO: Add the required input and textarea form elements.
+  // TODO: Add the required submit and change handlers
+
+  return (
+    <form name="create" onSubmit={handleSubmit}>
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <label htmlFor="name">
+                <input
+                  id="name"
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  value={NewRecipe.name}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="cuisine">
+                <input
+                  id="cuisine"
+                  type="text"
+                  name="cuisine"
+                  placeholder="Cuisine"
+                  value={NewRecipe.cuisine}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="photo">
+                <input
+                  id="photo"
+                  type="url"
+                  name="photo"
+                  placeholder="URL"
+                  value={NewRecipe.photo}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="ingredients">
+                <textarea
+                  id="ingredients"
+                  name="ingredients"
+                  placeholder="Ingredients"
+                  value={NewRecipe.ingredients}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </td>
+            <td>
+              <label htmlFor="preparation">
+                <textarea
+                  id="preparation"
+                  type="text"
+                  name="preparation"
+                  placeholder="Preparation"
+                  value={NewRecipe.preparation}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+            </td>
+            <td>
+              <button type="submit">Create</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </form>
+  );
+}
+
+export default RecipeCreate;
---
+++ src/RecipeList.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+function RecipeList({ recipes, setRecipes }) {
+  // TODO: Display the list of recipes using the structure of table that is provided.
+  // TODO: Create at least one additional component that is used by this component.
+  // TODO: Each recipe row must have a delete button - <button name="delete">Delete</button> - that deletes the post when clicked
+
+  const deleteRecipe = (indexToDelete) => {
+    return setRecipes(recipes.filter((_, index) => index !== indexToDelete)); // filters and removes desired index to delete
+  };
+
+  const rows = recipes.map(
+    // maps through each ingridient index and adds the data from each index in the table as its own row.
+    ({ name, cuisine, photo, ingredients, preparation }, index) => (
+      <tr key={index}>
+        <td>{name}</td>
+        <td>{cuisine}</td>
+        <td>
+          <img src={photo} alt="recipe" />
+        </td>
+        <td className="content_td">
+          <p>{ingredients}</p>
+        </td>
+        <td className="content_td">
+          <p>{preparation}</p>
+        </td>
+        <td>
+          <button name="delete" onClick={() => deleteRecipe(index)}>
+            delete
+          </button>
+        </td>
+      </tr>
+    )
+  );
+
+  return (
+    <div className="recipe-list">
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Cuisine</th>
+            <th>Photo</th>
+            <th>Ingredients</th>
+            <th>Preparation</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    </div>
+  );
+}
+
+export default RecipeList;
